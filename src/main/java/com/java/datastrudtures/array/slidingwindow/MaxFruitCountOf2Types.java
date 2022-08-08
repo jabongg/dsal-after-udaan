a160e4156c1afb0602b9f8f46e9323cfc7edd7c7
jpatel9614 <98873780+jpatel9614@users.noreply.github.com>
2022-08-08 22:49:39 +0530

    window sliding practice

---
+++ src/main/java/com/java/datastrudtures/array/slidingwindow/MaxFruitCountOf2Types.java
@@ -0,0 +1,89 @@
+package com.java.datastrudtures.array.slidingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Problem Statement#
+ *
+ * You are visiting a farm to collect fruits. The farm has a single row of fruit trees. You will be given two baskets, and your goal is to pick as many fruits as possible to be placed in the given baskets.
+ *
+ * You will be given an array of characters where each character represents a fruit tree. The farm has following restrictions:
+ *
+ *     Each basket can have only one type of fruit. There is no limit to how many fruit a basket can hold.
+ *     You can start with any tree, but you can’t skip a tree once you have started.
+ *     You will pick exactly one fruit from every tree until you cannot, i.e., you will stop when you have to pick from a third fruit type.
+ *
+ * Write a function to return the maximum number of fruits in both baskets.
+ *
+ * Example 1:
+ *
+ * Input: Fruit=['A', 'B', 'C', 'A', 'C']
+ * Output: 3
+ * Explanation: We can put 2 'C' in one basket and one 'A' in the other from the subarray ['C', 'A', 'C']
+ *
+ * Example 2:
+ *
+ * Input: Fruit=['A', 'B', 'C', 'B', 'B', 'C']
+ * Output: 5
+ * Explanation: We can put 3 'B' in one basket and two 'C' in the other basket.
+ * This can be done if we start with the second letter: ['B', 'C', 'B', 'B', 'C']
+ *
+ */
+public class MaxFruitCountOf2Types {
+
+    public static int findLength(String str, int k) {
+        if (str == null || str.length() == 0)
+            throw new IllegalArgumentException();
+
+        int windowStart = 0, maxLength = 0;
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+        // in the following loop we'll try to extend the range [windowStart, windowEnd]
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            charFrequencyMap.put(rightChar, charFrequencyMap.getOrDefault(rightChar, 0) + 1);
+            // shrink the sliding window, until we are left with 'k' distinct characters in the frequency map
+            while (charFrequencyMap.size() > k) {
+                char leftChar = str.charAt(windowStart);
+                charFrequencyMap.put(leftChar, charFrequencyMap.get(leftChar) - 1);
+                if (charFrequencyMap.get(leftChar) == 0) {
+                    charFrequencyMap.remove(leftChar);
+                }
+                windowStart++; // shrink the window
+            }
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1); // remember the maximum length so far
+        }
+
+        return maxLength;
+    }
+
+
+    public static int findLength(char[] arr) {
+        int windowStart = 0, maxLength = 0;
+        Map<Character, Integer> fruitFrequencyMap = new HashMap<>();
+        // try to extend the range [windowStart, windowEnd]
+        for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+            fruitFrequencyMap.put(arr[windowEnd], fruitFrequencyMap.getOrDefault(arr[windowEnd], 0) + 1);
+            // shrink the sliding window, until we are left with '2' fruits in the frequency map
+            while (fruitFrequencyMap.size() > 2) {
+                fruitFrequencyMap.put(arr[windowStart], fruitFrequencyMap.get(arr[windowStart]) - 1);
+                if (fruitFrequencyMap.get(arr[windowStart]) == 0) {
+                    fruitFrequencyMap.remove(arr[windowStart]);
+                }
+                windowStart++; // shrink the window
+            }
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Maximum number of fruits: " +
+                MaxFruitCountOf2Types.findLength(new char[] { 'A', 'B', 'C', 'A', 'C' }));
+        System.out.println("Maximum number of fruits: " +
+                MaxFruitCountOf2Types.findLength(new char[] { 'A', 'B', 'C', 'B', 'B', 'C' }));
+        System.out.println("Length of the longest substring: " + LongestSubstringKDistinct.findLength("araaci", 2));
+
+    }
+}
